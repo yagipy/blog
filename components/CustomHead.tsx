cb2b8107a048af5b2ccaf313c64501a7dc1f73e6
HiroyukiYagihashi <h.yagihashi2424@icloud.com>
2021-01-31 15:19:41 +0900

    Modify OGP image

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -20,15 +20,15 @@ export const CustomHead = ({ title, description, image, url }: Props): JSX.Eleme
       <meta property="og:description" content={description} />
       {/*<meta property="og:type" content="blog" />*/}
       {/*<meta property="og:url" content={url} />*/}
-      <meta property="og:image" content={`https://og-image.hiroyukiyagihashi.vercel.app/テスト.png`} />
-      <meta property="og:image:secure_url" content={`https://og-image.hiroyukiyagihashi.vercel.app/テスト.png`} />
+      <meta property="og:image" content={`https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`} />
+      <meta property="og:image:secure_url" content={`https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`} />
       <meta property="og:site_name" content={title} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@hryk_yg" />
       {/*<meta name="twitter:url" content={image} />*/}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`https://og-image.hiroyukiyagihashi.vercel.app/テスト.png`} />
+      <meta name="twitter:image" content={`https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       {/*<link rel="canonical" href={url} />*/}
