25b630d0b2e04cb6a5174971b2b4eb4330b42f1d
HiroyukiYagihashi <h.yagihashi2424@icloud.com>
2021-02-07 16:21:02 +0900

    Add created at column in article

--- components/Article.tsx
+++ components/Article.tsx
@@ -3,6 +3,7 @@ import {FC} from "react";
 import {TagList} from "./TagList";
 import {Toc} from "./Toc";
 import {HistoryList} from "./HistoryList";
+import {toDate} from "../utils/date";
 
 type Props = {
   slug: string
@@ -10,14 +11,16 @@ type Props = {
   history: any
   toc: any
   title: any
+  createdAt: number
   tags: any
   children: any
 };
 
-export const Article: FC<Props> = ({slug, ssgConfig, history, toc, title, tags, children}: Props) => {
+export const Article: FC<Props> = ({slug, ssgConfig, history, toc, title, createdAt, tags, children}: Props) => {
   return (
     <>
       <h1 className="text-4xl font-bold">{title}</h1>
+      <p className="text-gray-700">{`created at ${toDate(createdAt.toString())}`}</p>
       <TagList tags={tags} />
       <Toc toc={toc} slug={slug}/>
       {children}
