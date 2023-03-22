8f68ae0567c2e422fc63abecd16f307a0a1f3ee7
yagipy <yo@yagipy.me>
2023-03-22 14:05:53 +0900

    Change Profile and Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ type Props = {};
 export const Footer: FC<Props> = (props) => {
   return (
     <footer className="border-gray-500 border-t mt-3 mb-13 py-2">
-      <small className="text-gray-800">© Hiroyuki Yagihashi</small>
+      <small className="text-gray-800">© Hiroyuki YAGIHASHI</small>
     </footer>
   );
 };
