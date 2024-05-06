c38d15b911de5a9a4b943fe6db86aca37c0e24ce
yagipy <email@yagipy.me>
2024-05-06 14:16:17 +0900

    update

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export const CustomHead = ({ title, description, path }: Props): JSX.Element => {
-  const imageUrl = `https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`;
+  // const imageUrl = `https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`;
 
   return (
     <Head>
@@ -23,17 +23,17 @@ export const CustomHead = ({ title, description, path }: Props): JSX.Element => 
       <meta property="og:description" content={description} />
       {/*<meta property="og:type" content="blog" />*/}
       {/*<meta property="og:url" content={url} />*/}
-      <meta property="og:image" content={imageUrl} />
-      <meta property="og:image:secure_url" content={imageUrl} />
+      {/*<meta property="og:image" content={imageUrl} />*/}
+      {/*<meta property="og:image:secure_url" content={imageUrl} />*/}
       <meta property="og:site_name" content={"hiroyuki blog"} />
-      <meta name="twitter:card" content="summary_large_image" />
+      {/*<meta name="twitter:card" content="summary_large_image" />*/}
       <meta name="twitter:site" content="@yagipy" />
       {/*<meta name="twitter:url" content={image} />*/}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
+      {/*<meta name="twitter:image" content={`https://og-image.hiroyukiyagihashi.vercel.app/${title}.png`} />*/}
+      {/*<meta property="og:image:width" content="1200" />*/}
+      {/*<meta property="og:image:height" content="630" />*/}
       <link rel="canonical" href={`${_config.host}${path}`} />
       <link rel="shortcut icon" href={'/favicon.ico'} />
       <link rel="apple-touch-icon" href={'/icon-192x192.png'} />
